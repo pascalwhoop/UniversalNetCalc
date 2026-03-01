61bc0ab4e38f0660b4dcef9d969863dad8e5d85a
Pascal Brokmeier <git@pascalbrokmeier.de>
2026-03-01 23:21:23 +0100

    bugfix: cloudflare build

---
+++ src/data/changelog/index.ts
@@ -0,0 +1,12 @@
+import type { ChangelogData } from "@/types/changelog"
+import v0211 from "./0.2.11.json"
+import v0210 from "./0.2.10.json"
+import v029 from "./0.2.9.json"
+import v028 from "./0.2.8.json"
+import v027 from "./0.2.7.json"
+import v024 from "./0.2.4.json"
+import v020 from "./0.2.0.json"
+
+export const changelog: ChangelogData = {
+  versions: [v0211, v0210, v029, v028, v027, v024, v020]
+}
