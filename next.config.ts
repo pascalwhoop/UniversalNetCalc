c81e63833e16362b019c17d0745c9752134b017d
Pascal Brokmeier <git@pascalbrokmeier.de>
2026-03-04 00:34:19 +0100

    chore: revert next.js version attempts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	serverExternalPackages: ["preact-render-to-string"],
+	/* config options here */
 };
 
 export default nextConfig;
